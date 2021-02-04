275aa131aa0caa6eeaa5c23f5d54129e228dddf0
alireza-nasirian <an1380an@gmail.com>
2021-02-04 23:38:40 +0330

    fix: correct linked list

--- main.c
+++ main.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <math.h>
+
 
 int x_size = 10, y_size = 10;
 
 typedef struct Node{
     bool ship;      //true: when this node contains ship - false: this node doesn't contain ship
     char state;    // ' ':Unknown - 'w' = water - e = exploded - 'c' = completely  exploded
-    int x,y;
+    int key;
     struct Node *next;
 }node;
 
 node *head1 = NULL;
 node *head2 = NULL;
 
-void insert(int y,int x,bool ship,char state,node** head){
+void insert(int key,bool ship,char state,node** head){
     node *link = (node*)malloc(sizeof(node));
-    link->x = x;
-    link->y = y;
+    link->key = key;
     link->ship = ship;
     link->state = state;
     link->next = *head;
@@ -28,18 +27,19 @@ void insert(int y,int x,bool ship,char state,node** head){
 
 void creat_map(node **head) {
     for (int i = 0; i < (x_size * y_size); i++) {
-        insert((i / 10) + 1, (i % 10) + 1, false, ' ',head);
+        insert(i+1, false, ' ',head);
     }
 }
 
-node* find(node *head,int y,int x) {
+node* find(node **head,int y,int x) {
+    int key = ((y-1)*10) + x;
 
-    node* current = head;
+    node* current = *head;
 
-    if(head == NULL) {
+    if(*head == NULL) {
         return NULL;
     }
-    while((current->y != y)&&(current->x != x)) {
+    while(current->key != key) {
 
         if(current->next == NULL) {
             return NULL;
@@ -49,8 +49,3 @@ node* find(node *head,int y,int x) {
     }
     return current;
 }
-
-
-
-
-
